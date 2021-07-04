502769800c652dd7810bc6d3bebec880ec8120e3
Abdullah Ridwan <abdullahridwan73@icloud.com>
2021-07-04 15:04:30 -0400

    Initial Commit

---
+++ Shared/runningLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  runningLogApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct runningLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
